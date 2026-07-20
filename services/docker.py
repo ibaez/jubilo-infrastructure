0dc1f53a232027ba94d5dc2971e8782b199df06e
Ismael Baez <ismaelbaez@ib-black.local>
2026-07-20 18:51:07 -0500

    changes to support rename from iece_auth to jubilo_auth

--- services/docker.py
+++ services/docker.py
@@ -9,11 +9,11 @@ def docker_generate_auth_service_env_file(docker_host_ip, tokens):
 		f.write(f"DJANGO_SUPERUSER_PASSWORD={tokens['auth']['DJANGO_SUPERUSER_PASSWORD']}\n")
 		f.write(f"DJANGO_SUPERUSER_FIRST_NAME={tokens['auth']['DJANGO_SUPERUSER_FIRST_NAME']}\n")
 		f.write(f"DJANGO_SUPERUSER_LAST_NAME={tokens['auth']['DJANGO_SUPERUSER_LAST_NAME']}\n")
-		f.write(f"IECE_GATEWAY_IP={docker_host_ip}\n")
-		f.write(f"IECE_AUTH_CLIENT_ID={tokens['auth']['client_id']}\n")
-		f.write(f"IECE_AUTH_CLIENT_SECRET={tokens['auth']['client_secret']}\n")
-		f.write(f"IECE_CHURCH_CLIENT_ID={tokens['church']['client_id']}\n")
-		f.write(f"IECE_CHURCH_CLIENT_SECRET={tokens['church']['client_secret']}\n")
+		f.write(f"JUBILO_GATEWAY_IP={docker_host_ip}\n")
+		f.write(f"JUBILO_AUTH_CLIENT_ID={tokens['auth']['client_id']}\n")
+		f.write(f"JUBILO_AUTH_CLIENT_SECRET={tokens['auth']['client_secret']}\n")
+		f.write(f"JUBILO_CHURCH_CLIENT_ID={tokens['church']['client_id']}\n")
+		f.write(f"JUBILO_CHURCH_CLIENT_SECRET={tokens['church']['client_secret']}\n")
 		f.write(f"JUBILO_MOBILE_CLIENT_ID={tokens['jubilo_mobile']['client_id']}\n")
 		f.write(f"JUBILO_MOBILE_CLIENT_SECRET={tokens['jubilo_mobile']['client_secret']}\n")
 		f.write(f"JUBILO_MUSIC_CLIENT_ID={tokens['jubilo_music']['client_id']}\n")
@@ -36,15 +36,15 @@ def docker_generate_music_service_env_file(docker_host_ip, tokens):
 
 def docker_generate_church_service_env_file(docker_host_ip, tokens):
 	with open("../iece-church/.env", "w") as f:
-		f.write(f"IECE_GATEWAY_IP={docker_host_ip}\n")
-		f.write(f"IECE_CHURCH_CLIENT_ID={tokens['client_id']}\n")
-		f.write(f"IECE_CHURCH_CLIENT_SECRET={tokens['client_secret']}\n")
+		f.write(f"JUBILO_GATEWAY_IP={docker_host_ip}\n")
+		f.write(f"JUBILO_CHURCH_CLIENT_ID={tokens['client_id']}\n")
+		f.write(f"JUBILO_CHURCH_CLIENT_SECRET={tokens['client_secret']}\n")
 
 def docker_generate_jubilo_mobile_env_file(docker_host_ip, tokens):
 	with open("../jubilo-mobile/.env", "w") as f:
-		f.write(f"IECE_GATEWAY_IP={docker_host_ip}\n")
-		f.write(f"IECE_CHURCH_CLIENT_ID={tokens['client_id']}\n")
-		f.write(f"IECE_CHURCH_CLIENT_SECRET={tokens['client_secret']}\n")
+		f.write(f"JUBILO_GATEWAY_IP={docker_host_ip}\n")
+		f.write(f"JUBILO_CHURCH_CLIENT_ID={tokens['client_id']}\n")
+		f.write(f"JUBILO_CHURCH_CLIENT_SECRET={tokens['client_secret']}\n")
 
 def generate_token(length):
 	while True:
